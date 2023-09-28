60fb342f514001ad3ea766df018b76ef5ce945f5
manlikevee <odahviktor@gmail.com>
2023-09-29 00:17:05 +0100

    vee

---
+++ veebankapp/users/serializer.py
@@ -0,0 +1,13 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Customize the token data here
+        token['username'] = user.username
+        token['email'] = user.email
+
+        return token
--- veebankapp/users/views.py
+++ veebankapp/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
 
 
